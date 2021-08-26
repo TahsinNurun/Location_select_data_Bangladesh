fc35b89487f59c17df39ad804a9f9cb56cc3cd27
Tahsin Nurun <nuruntahsin@gmail.com>
2021-08-26 09:40:32 +0600

    thana data banlgla dhaka khulna mymensing

--- thana.js
+++ thana.js
@@ -1,160 +1,160 @@
-{
-    "upazila": "",
-        "thana": [
-            {
-                "name": ""
-            },
-            {
-                "name": ""
-            },
-            {
-                "name": ""
-            },
-            {
-                "name": ""
-            },
-            {
-                "name": ""
-            },
-            {
-                "name": ""
-            }
-        ]
+[{
+    "upazila": "Amtoli",
+    "thana": [
+        {
+            "name": "Amtoli"
+        },
+        {
+            "name": ""
+        },
+        {
+            "name": ""
+        },
+        {
+            "name": ""
+        },
+        {
+            "name": ""
+        },
+        {
+            "name": ""
+        }
+    ]
 },
 {
-    "upazila": "",
-        "thana": [
-            {
-                "name": ""
-            },
-            {
-                "name": ""
-            },
-            {
-                "name": ""
-            },
-            {
-                "name": ""
-            },
-            {
-                "name": ""
-            },
-            {
-                "name": ""
-            }
-        ]
+    "upazila": "Taltoli",
+    "thana": [
+        {
+            "name": ""
+        },
+        {
+            "name": ""
+        },
+        {
+            "name": ""
+        },
+        {
+            "name": ""
+        },
+        {
+            "name": ""
+        },
+        {
+            "name": ""
+        }
+    ]
 }, {
-    "upazila": "",
-        "thana": [
-            {
-                "name": ""
-            },
-            {
-                "name": ""
-            },
-            {
-                "name": ""
-            },
-            {
-                "name": ""
-            },
-            {
-                "name": ""
-            },
-            {
-                "name": ""
-            }
-        ]
-}, 
-{
-    "upazila": "",
-        "thana": [
-            {
-                "name": ""
-            },
-            {
-                "name": ""
-            },
-            {
-                "name": ""
-            },
-            {
-                "name": ""
-            },
-            {
-                "name": ""
-            },
-            {
-                "name": ""
-            }
-        ]
+    "upazila": "Patharghata",
+    "thana": [
+        {
+            "name": ""
+        },
+        {
+            "name": ""
+        },
+        {
+            "name": ""
+        },
+        {
+            "name": ""
+        },
+        {
+            "name": ""
+        },
+        {
+            "name": ""
+        }
+    ]
 },
- {
-    "upazila": "",
-        "thana": [
-            {
-                "name": ""
-            },
-            {
-                "name": ""
-            },
-            {
-                "name": ""
-            },
-            {
-                "name": ""
-            },
-            {
-                "name": ""
-            },
-            {
-                "name": ""
-            }
-        ]
-}, 
 {
-    "upazila": "",
-        "thana": [
-            {
-                "name": ""
-            },
-            {
-                "name": ""
-            },
-            {
-                "name": ""
-            },
-            {
-                "name": ""
-            },
-            {
-                "name": ""
-            },
-            {
-                "name": ""
-            }
-        ]
-}, 
-{
-    "upazila": "",
-        "thana": [
-            {
-                "name": ""
-            },
-            {
-                "name": ""
-            },
-            {
-                "name": ""
-            },
-            {
-                "name": ""
-            },
-            {
-                "name": ""
-            },
-            {
-                "name": ""
-            }
-        ]
+    "upazila": "Barguna Sadar",
+    "thana": [
+        {
+            "name": ""
+        },
+        {
+            "name": ""
+        },
+        {
+            "name": ""
+        },
+        {
+            "name": ""
+        },
+        {
+            "name": ""
+        },
+        {
+            "name": ""
+        }
+    ]
 },
+{
+    "upazila": "Bamna",
+    "thana": [
+        {
+            "name": ""
+        },
+        {
+            "name": ""
+        },
+        {
+            "name": ""
+        },
+        {
+            "name": ""
+        },
+        {
+            "name": ""
+        },
+        {
+            "name": ""
+        }
+    ]
+},
+{
+    "upazila": "Betagi",
+    "thana": [
+        {
+            "name": ""
+        },
+        {
+            "name": ""
+        },
+        {
+            "name": ""
+        },
+        {
+            "name": ""
+        },
+        {
+            "name": ""
+        },
+        {
+            "name": ""
+        }
+    ]
+},
+{
+    "upazila": "",
+    "thana": [
+        {
+            "name": ""
+        },
+        {
+            "name": ""
+        },
+        {
+            "name": ""
+        },
+        {
+            "name": ""
+        },
+        {
+            "name": ""
+        },
+        {
+            "name": ""
+        }
+    ]
+}]
